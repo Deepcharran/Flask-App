b32b7cb4ed373fdb30f96c5b55b3a4820db39a96
Deepcharran N (dneminat) <dneminat@cisco.com>
2021-03-27 11:28:45 +0530

    first commit

--- main.py
+++ main.py
@@ -4,78 +4,56 @@ from influxdb import InfluxDBClient
 application = Flask(__name__)
 def influxdbfun():
 	client = InfluxDBClient(host='10.106.79.198',port=8086)
-	client.switch_database('telegraf')
-	data = client.query('SELECT "source", "neighbor_address_xr/ipv4_address", "is_this_neighbor_us", "interface_name" FROM "Cisco-IOS-XR-ipv4-pim-oper:pim/active/default-context/neighbors/neighbor" WHERE time > now() - 30s GROUP BY "source"')
+	client.switch_database('telegraf') 
+	data = client.query('SELECT "source", "neighbor_address_xr/ipv4_address" AS IP, "is_this_neighbor_us", "interface_name" FROM "Cisco-IOS-XR-ipv4-pim-oper:pim/active/default-context/neighbors/neighbor" WHERE time > now() - 30s GROUP BY "source"')
 	routers=[]
 	routernames=[]
+	unrouternames=[]
 	results=[]
-		#ipdict = {}
+	ipmap = {}
+	#intmap = {}
+	unidenno=1
 	for i in data:
 		for j in i:
-			#j['target']='target'
 			for k,v in j.items():
-				if k == 'neighbor_address_xr/ipv4_address' and v.startswith("192.184.163"):
+				if k == 'IP' and v.startswith("192.184.163"):
 					results.append(j)
 	for j in results:
-		#j['target']='target'
 		for k,v in j.items():
 			if k == 'source' and v not in routers:
 				routers.append(v)
 				routernames.append({"id": v}) 
-		#for k,v in j.items():
-		#    if k=='is_this_neighbor_us' and v == 'true':
-		#        ipdict[j['interface_name']]=j['source']
+		for k,v in j.items():
+			if k=='is_this_neighbor_us' and v == 'true':
+				ipmap[j['IP']]=j['source']
+				#intmap[j['interface_name']]=j['source']
 	for j in results:
-		#if j['interface_name'] in ipdict:
-		#    j['target']=ipdict[j['interface_name']]  
-		if j['neighbor_address_xr/ipv4_address'] == '192.184.163.5':
-			j['target']='ASR-9906-B'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.9':
-			j['target']='ASR-9906-B'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.22':
-			j['target']='ASR-9906-B'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.2':
-			j['target']='ASR9906-A'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.6':
-			j['target']='ASR9906-A'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.1':
-			j['target']='NCS-5508-A'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.14':
-			j['target']='NCS-5508-A'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.18':
-			j['target']='NCS-5508-A'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.10':
-			j['target']='ASR-9904-G-LS'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.17':
-			j['target']='ASR-9904-G-LS'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.26':
-			j['target']='ASR-9904-G-LS'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.21':
-			j['target']='ASR-9906-C-LS'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.25':
-			j['target']='ASR-9906-C-LS'
-		elif j['neighbor_address_xr/ipv4_address'] == '192.184.163.13':
-			j['target']='NCS-5508-B'
-		
-	#print("Total Numbers of Active Routers :",len(routernames))
+		if j['IP'] in ipmap and j['is_this_neighbor_us']=='false': # and j['interface_name'] in intmap:
+			j['target'] = ipmap[j['IP']]  
+		elif j['IP'] not in ipmap and j['is_this_neighbor_us']=='false':
+			j['target']='Unidentified Router'+str(unidenno)
+			unidenno=unidenno+1    
+	# print("Total Numbers of Known Active Routers :",len(routernames))
 	for j in results:
 		for k,v in j.items():
 			if k == 'target' and v not in routers:
-				routers.append(v)
-				routernames.append({"id": v})
-	# print("Nodes(Routers) :",routernames)
+				unrouternames.append({"id": v})
+	# print("Total Numbers of Unknown Active Routers :",len(unrouternames))
+	# print("Nodes(Known Routers) :",routernames)
 	# print("\n")
 	detailed_results=results
-	# print(detailed_results)
 	for i in results:
 		del i['time']
-		del i['neighbor_address_xr/ipv4_address']
+		del i['IP']
 		del i['interface_name']
 		del i['is_this_neighbor_us']
-
-	# print("\n")
 	# print(results)
-	return (routernames, results)
+	ans=[]
+	for j in results:
+		if len(j)==2:
+			ans.append(j)
+	print(ans)
+	return (routernames, ans)
 
 
 
